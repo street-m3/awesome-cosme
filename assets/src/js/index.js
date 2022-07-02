cb1295eb6831e35b9149bdf3837d082978613d99
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2022-07-02 17:19:38 +0900

    [Add] import link{template}.js

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,7 +1,8 @@
 'use strict';
-
+import './components/site/link{template}';
 import './env/enabled';
 import './router';
 import './components/site/header{template}';
 import './components/site/footer{template}';
 import './env/viewport-scale';
+// import './lib/carousel';
