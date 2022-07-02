b64c71dcb43a290452ccf6c3e984e63d429f3cf9
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2022-07-02 17:21:11 +0900

    [Add] 404page

--- assets/src/js/pages/404.js
+++ assets/src/js/pages/404.js
@@ -2,6 +2,10 @@ import { LitElement, html, css } from 'lit';
 import { presets } from '../env/env';
 import reset from '../styles/reset';
 import container from '../styles/container';
+import headline from '../styles/headline';
+import typography from '../styles/typography';
+import utils from '../styles/utils';
+
 
 const tagName = 'pages-error';
 
@@ -10,6 +14,10 @@ export class Error extends LitElement {
         return [
             reset, 
             container, 
+            headline,
+            typography,
+            utils,
+            css``
         ]
     }
 
@@ -19,7 +27,15 @@ export class Error extends LitElement {
 
     render() {
         return html`
-        <h1>テスト 404ページ</h1>
+        <section class="pages-Singular-ComponentBlock" style="padding: clamp(100px, 6.25rem + (1vw - 4.14px) * 5.2521, 150px) 0;">
+            <div class="ly-Inner_Grid -lg">
+                <div class="c-headline-lv6 u-uppercase">404 Error</div>
+                <h2 class="c-headline-lv2" style="margin-top: 40px; margin-bottom: clamp(100px, calc(6.25rem + ((1vw - 4.14px) * 5.2521)), 150px);">404 Not Found</h2>
+                <div class="pages-Singular-Component_Content o-userWriting-Area">
+                    <p>申し訳ございません。ページが見つかりませんでした。</p>
+                </div>
+            </div>
+        </section>
         `;
     }
 }
