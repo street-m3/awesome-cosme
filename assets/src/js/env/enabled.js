7757b3a3eab5800ed22b40688fc617c201f33aca
Junichi-Takabatake <65938603+street-m3@users.noreply.github.com>
2022-09-20 12:16:49 +0900

    [Update] feat(*)

--- assets/src/js/env/enabled.js
+++ assets/src/js/env/enabled.js
@@ -1,7 +1,8 @@
 const EnabledScripts = () => {
+    const mode = false;
     let Html = document.documentElement;
 
-    if (Html.nodeType) {
+    if (Html.nodeType && mode) {
         Html.setAttribute('data-js-enabled', 'true');
         console.info('JavaScript is loading properly!');
     }
